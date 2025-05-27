a8a4871b31765b99265e873272534dff737b7146
gauravrize <gaurav@vrize.com>
2025-05-27 19:13:59 +0530

    Action class added

---
+++ src/test/java/com/tonic/actions/HomeActions.java
@@ -0,0 +1,9 @@
+package com.tonic.actions;
+
+
+/**
+ * Contains user authentication-related actions such as login, logout,
+ * and navigation from Login to Home page.
+ */
+public class HomeActions {
+}
